0b36ffb127a078014292ad507f31109ccc24e2f1
Utkarshgaut <134734648+Utkarshgaut@users.noreply.github.com>
2024-12-06 03:04:59 +0530

    6/12/24

--- frontend/src/helpers/api-communicator.tsx
+++ frontend/src/helpers/api-communicator.tsx
@@ -8,3 +8,12 @@ export const loginUser = async (email:string,password: string) => {
     const data = await res.data;
     return data;
 };
+
+export const checkAuthStatus = async () => {
+    const  res = await axios.post("/user/auth-status");
+    if(res.status !== 200){
+        throw new Error("Unable to aunthenticate");
+    }
+    const data = await res.data;
+    return data;
+};
